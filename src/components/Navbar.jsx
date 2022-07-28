3dedbae8431efc60063bd6b38254fa52e78a79c9
özcan çetin <cetin445@gmail.com>
2022-07-28 18:59:58 +0300

    login style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
+import {useNavigate} from "react-router-dom"
 
 const Navbar = () => {
-  const [currentUser, setCurrentUser] = useState(true);
+  const [currentUser, setCurrentUser] = useState(false);
+  const navigate = useNavigate()
   return (
     <nav className="navbar navbar-expand-lg bg-info gap-2 d-md-flex justify-content-end">
       {currentUser ? (
         <button className="btn btn-primary me-3">Logout</button>
       ) : (
         <>
-          <button className="btn btn-primary">Login</button>
-          <button className="btn btn-primary me-3">Register</button>
+          <button className="btn btn-primary" onClick={()=>navigate("/login")}>Login</button>
+          <button className="btn btn-primary me-3" onClick={()=>navigate("/register")}>Register</button>
         </>
       )}
     </nav>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
   return (
-    <div>Login</div>
+    <div className='d-flex row'>
+        <div className='d-none d-md-block col-6'>
+        <img src={'https://picsum.photos/700/700'} alt="sample-movie" />
+        </div>
+        <div className='ms-5 col-10 col-md-5'>
+            <h1>Login Form</h1>
+            <form>
+                <div className='input-group mb-3 gap-4'>
+                    <label htmlFor="email" className="form-label col-1 me-3">Email</label>
+                    <input type="email" className='form-control' placeholder="Enter Your Email Adress" required value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                </div>
+                <div className='input-group mb-3 gap-4'>
+                    <label htmlFor="password" className="form-label col-1 me-3">Password</label>
+                    <input type="password" className='form-control' placeholder="Enter Your Password" required value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                </div>
+               
+            </form>
+        </div>
+    </div>
   )
 }
 
