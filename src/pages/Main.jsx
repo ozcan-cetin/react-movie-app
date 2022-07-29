216f0993e5d4d04f91e2eb7dd67d731e311a5fb7
özcan çetin <cetin445@gmail.com>
2022-07-29 13:32:42 +0300

    map function

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react'
+import MovieCard from '../components/MovieCard';
+import { AuthContext } from '../context/AuthContext';
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
 const Main = () => {
+  const [movies, setMovies] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const {currentUser} = useContext(AuthContext)
+  
+
+  const getMovies = async(API) => {
+    setIsLoading(true)
+    try {
+      const {data} = await axios.get(API)
+      console.log(data);
+      setMovies(data.results)
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    getMovies(FEATURED_API)
+  }, [])
+  
+  if (isLoading){
+    <h1>Loading...</h1>
+  }
   return (
-    <div>Main</div>
+    <div>
+      <div>
+        {movies?.map((movie)=><MovieCard key={movie.id} movie={movie}/>)}
+      </div>
+    </div>
   )
 }
 
