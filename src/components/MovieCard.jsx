0237acfcccb99c4cc9599c9afabeee99c2b13d19
özcan çetin <cetin445@gmail.com>
2022-07-29 18:52:53 +0300

    vote average

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useContext} from 'react'
 import {useNavigate} from "react-router-dom"
+import { AuthContext } from '../context/AuthContext';
 
 const MovieCard = ({movie}) => {
   const {id, title, vote_average, overview, poster_path} = movie;
   const navigate = useNavigate()
+  const {currentUser} = useContext(AuthContext)
 
   const IMG_API = 'https://image.tmdb.org/t/p/w1280';
 
   const defaultImage =
   'https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80';
 
+  const setVoteClass = (vote) => {
+    if(vote > 8){
+      return "green"
+    }
+    else if(vote >= 6){
+      return "orange"
+    }else{
+      return "red"}
+  }
+
   return (
     <div className='singleMovie' onClick={()=>navigate("/details/" + id)}>
       <div className='movie-img'>
         <img src={poster_path ? IMG_API + poster_path : defaultImage} alt={title} />
       </div>
-      <div className='movieTitle'>
-        <h3>{title}</h3>
+      <div className='movieTitle d-flex justify-content-between px-2'>
+        <h2>{title}</h2>
+        {currentUser && <h3 className={`tag ${setVoteClass(vote_average)}`}>{vote_average}</h3> }
       </div>
       <div className='overview'>
         <p>{overview}</p>
