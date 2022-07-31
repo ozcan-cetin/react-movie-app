7efe8e40e2a0442e5d4cd3bf614fbe7613e696e9
özcan çetin <cetin445@gmail.com>
2022-07-31 18:20:02 +0300

    movie detail page

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 const MovieDetail = () => {
+  const [movieDetails, setMovieDetails] = useState("")
   return (
-    <div>MovieDetail</div>
-  )
-}
+    <div>
+      <div className='card mb-3" style="max-width: 540px;'>
+        <div className="row g-0">
+          <div className="col-md-4">
+            <img src="" className="img-fluid rounded-start" alt="" />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
 
-export default MovieDetail
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieDetail;
