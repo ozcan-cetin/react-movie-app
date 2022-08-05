74c9f67e8a165f93d431d7af9f130b2db56c595b
özcan çetin <cetin445@gmail.com>
2022-08-06 02:07:14 +0300

    toastify v2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import AuthContextProvider from './context/AuthContext';
 import AppRouter from './router/AppRouter';
-// import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div>
       <AuthContextProvider>
       <AppRouter/>
-      {/* <ToastContainer /> */}
+      <ToastContainer />
       </AuthContextProvider>
     </div>
   );
